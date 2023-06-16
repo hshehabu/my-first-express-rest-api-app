a1e6723bb0fcdbfecb9cf6ef382308a6cd6bef0f
hshehabu <hamzashehabu457@gmail.com>
2023-06-16 15:07:54 +0300

    get request handling

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, course: "amharic" },
+  { id: 2, course: "english" },
+  { id: 3, course: "biology" },
+];
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 app.get("/api/courses", (req, res) => {
   res.send([1, 3, 4, 54]);
 });
-app.get('/api/posts/:year/:date' , (req , res) =>{
-    // res.send(req.params);
-    res.send(req.query);
-})
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('no courses match with this id');
+  res.send(course)
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening at port ${port}`));
