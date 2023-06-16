27ff3a94e649a2dd8a7cc71b8d5afa905ac0e5a5
hshehabu <hamzashehabu457@gmail.com>
2023-06-16 18:07:26 +0300

    Put request

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const courses = [
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
-// app.get("/api/courses", (req, res) => {
-//   res.send([1, 3, 4, 54]);
-// });
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) res.status(404).send("no courses match with this id");
@@ -24,13 +24,10 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", function (req, res) {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-  const result = Joi.validate(req.body, schema);
+  const { error } = validate(req.body);
 
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
   const { name } = req.body;
@@ -42,5 +39,25 @@ app.post("/api/courses", function (req, res) {
   res.send(course);
 });
 
+app.put("/api/courses/:id", (req, res) => {
+  //look up the course
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  //if not
+  if (!course) res.status(404).send("doesn't match any course");
+  //if so
+  const { error } = validate(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  course.name = req.body.name;
+  res.send(course);
+});
+function validate(course) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  return Joi.validate(course, schema);
+}
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening at port ${port}`));
